2e6f01728d98a5c13a5c0d073463873599dea318
Aryan Agarwal <aryan23062001@gmail.com>
2024-12-05 09:58:39 +0530

    auth-changes

---
+++ app/register/page.tsx
@@ -0,0 +1,90 @@
+"use client";
+import { auth } from "@/firebase";
+import { useState } from "react";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { User } from "firebase/auth";
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [createUserWithEmailAndPassword] =
+    useCreateUserWithEmailAndPassword(auth);
+  const [user, setUser] = useState<User | null>(null);
+
+  const handleGoogleSignIn = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      const result = await signInWithPopup(auth, provider);
+      setUser(result.user);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error("Error message:", error.message);
+      } else {
+        console.error("Unknown error:", error);
+      }
+    }
+  };
+
+  const handleSignUp = async () => {};
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <div className="bg-gray-800 p-10 rounded-lg shadow-xl w-96">
+        <h1 className="text-white text-2xl mb-5">Sign Up</h1>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
+        />
+        <button
+          onClick={handleSignUp}
+          className="w-full p-3 bg-indigo-600 rounded text-white hover:bg-indigo-500"
+        >
+          Sign Up
+        </button>
+        <button
+          onClick={handleGoogleSignIn}
+          className="flex items-center justify-center w-full py-2 px-4 
+                 bg-white border border-gray-300 rounded-lg 
+                 shadow-md hover:shadow-lg transition duration-300 
+                 focus:outline-none focus:ring-2 focus:ring-blue-500 mt-10"
+        >
+          <svg
+            className="w-6 h-6 mr-2"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 48 48"
+          >
+            <path
+              fill="#4285F4"
+              d="M45.12 24.5c0-1.56-.14-3.06-.4-4.5H24v8.51h11.84c-.51 2.75-2.06 5.08-4.39 6.64v5.52h7.11c4.16-3.83 6.56-9.47 6.56-16.17z"
+            />
+            <path
+              fill="#34A853"
+              d="M24 46c5.94 0 10.92-1.97 14.56-5.33l-7.11-5.52c-1.97 1.32-4.49 2.1-7.45 2.1-5.73 0-10.58-3.87-12.32-9.07H4.34v5.7C7.96 41.07 15.4 46 24 46z"
+            />
+            <path
+              fill="#FBBC05"
+              d="M11.68 28.18c-.54-1.62-.85-3.35-.85-5.18s.31-3.56.85-5.18v-5.7H4.34C2.85 14.89 2 19.32 2 24s.85 9.11 2.34 12.88l7.34-5.7z"
+            />
+            <path
+              fill="#EA4335"
+              d="M24 9.75c3.23 0 6.13 1.11 8.41 3.29l6.31-6.31C34.91 3.29 29.93 1 24 1 15.4 1 7.96 5.93 4.34 14.12l7.34 5.7c1.74-5.2 6.59-9.07 12.32-9.07z"
+            />
+          </svg>
+          <span className="text-gray-700 font-medium">Sign in with Google</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
